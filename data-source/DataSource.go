e7adfd15e5d6799716df759f514db29da9510428
Krasimir Ivanov <krasimir.ivanov@coretrix.com>
2022-07-14 22:45:41 +0300

    fixing code style

--- data-source/DataSource.go
+++ data-source/DataSource.go
@@ -7,6 +7,5 @@ type DataSource interface {
 }
 
 func MicroTime(timer time.Time) float64 {
-	micSeconds := float64(timer.Nanosecond()) / float64(time.Second)
-	return float64(timer.Unix()) + micSeconds
+	return float64(timer.Unix()) + float64(timer.Nanosecond())/float64(time.Second)
 }
